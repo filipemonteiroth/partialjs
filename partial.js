c88f547a191981f6909cd4bd462fbc248241c1bd
Filipe Monteiro <filipe@tfsolutions.com.br>
2014-03-01 12:54:37 -0300

    Adding multiple partials feature

--- partial.js
+++ partial.js
@@ -2,6 +2,7 @@
 
 	{
 		file: "partials/content.html",
+		data: "<div>{{nome}}</div>",
 		values: {
 			name: "My partial"
 		}
@@ -25,20 +26,61 @@ function Partial(params) {
 
 	this.load = function(callback) {
 		this._completeCallback = callback;
-		var _this = this;		
-		$.get(this._params.file, function(data){
-			_this.process(data);
-		});
+		var _this = this;
+		if (this._params.data != undefined) {
+			this.process(this._params.data); 
+		} else {
+			$.get(this._params.file, function(data){
+				_this.process(data);
+			});
+		}		
 		return this;
 	};
 
 	this.process = function(partial) {
+		/* First it will process all subpartials if exist */
 		this._partialProcessed = partial;
+		this.processAllPartials();
 		for (var property in this._params.values) {
 			this.fill(property, this._params.values[property]);
 		}
 		this._completeCallback(this._partialProcessed);
 	};
+
+	this.processAllPartials = function() {
+		if (this._params.partials != undefined) {
+			for (var i = 0; i < this._params.partials.length; i++) {
+				this.loadAndProcessPartial(this._params.partials[i]);
+			}
+		}
+	}
+
+	this.loadAndProcessPartial = function(partial) {		
+		if (partial.data != undefined) {
+			this.processPartial(partial, partial.data);
+		} else {
+			var _this = this;
+			$.get(partial.file, function(data){
+				_this.processPartial(partial, data);
+			});
+		}		
+	}
+
+	this.processPartial = function(partial, data) {
+		var processed = "";
+		for (var i = 0; i < partial.values.length; i++) {
+			var preProcessed = data;
+			for (var property in partial.values[i]) {
+				preProcessed = this.fillWith(preProcessed, property, partial.values[i][property]);
+			}
+			processed += preProcessed;
+		}
+		this.fill(partial.name, processed);
+	};
+
+	this.fillWith = function(data, property, value) {
+		return data.replaceAll("{{" + property + "}}", value);
+	}
 
 	this.fill = function(property, value) {
 		this._partialProcessed = this._partialProcessed.replaceAll("{{" + property + "}}", value);
